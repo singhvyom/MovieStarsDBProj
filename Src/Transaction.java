109ccceb7f7d7f018c9f037ea6c983d5d3970b18
Vyom Singh <vyomsaritsingh@ucsb.edu>
2023-12-03 14:37:07 -0800

    Transaction Class

--- Src/Transaction.java
+++ Src/Transaction.java
@@ -2,4 +2,15 @@ package Src;
 
 public class Transaction {
     
+    public void deposit(){};
+    public void withdraw(){};
+    public void buy(){};
+    public void sell(){};
+    public void cancel(){};
+
+//     Amt added = monthly interest rate times the average daily
+//     balance for the month (e.g., an account with balance $30 for 10 days and $60 for 20 days in a 30-day
+//     month has an average daily balance of $50, not the simple average of $45!). Interest is added at the end
+//     of each month.
+    public void accrueInterest(){};
 }
