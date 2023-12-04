7c472fe420e0d90a3a9484ae5cdd01fc026076d9
Vyom Singh <vyomsaritsingh@ucsb.edu>
2023-12-04 00:23:08 -0800

    notes added

--- Src/DAO/TraderInterfaceDAO.java
+++ Src/DAO/TraderInterfaceDAO.java
@@ -1,6 +1,6 @@
-package Src;
+package Src.DAO;
 
-public interface TraderInterface {
+public interface TraderInterfaceDAO {
     //implementation in TraderInterfaceImpl.java
     void registerCustomer(String username, String password);
     boolean login(String username, String password);
@@ -9,11 +9,13 @@ public interface TraderInterface {
     void buy(String stockSymbol, int quantity);
     void sell(String stockSymbol, int quantity);
     void cancel(String transactionId);
-    double getMarketAccountBalance();
+    double showMarketAccountBalance();
     void showTransactionHistory();
     double getCurrentStockPrice(String stockSymbol);
     void showActorProfile(String actorName);
     void showMovieInformation(String movieTitle);
+    //these bottom two functions should be implemented in show movie information i think, 
+    //since its available by request
     void listTopMovies(int startYear, int endYear);
     void displayMovieReviews(String movieTitle);
 }
