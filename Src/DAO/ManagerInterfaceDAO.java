2ef2c3f4a54780ae599d9f9c87fdf4ddbb9849fb
Vyom Singh <vyomsaritsingh@ucsb.edu>
2023-12-05 00:28:35 -0800

    more functions implemented

--- Src/DAO/ManagerInterfaceDAO.java
+++ Src/DAO/ManagerInterfaceDAO.java
@@ -4,11 +4,11 @@ import Src.Customer;
 public interface ManagerInterfaceDAO {
     //functions for the manager interface
     public boolean login(String username, String password);
-    public void addInterest();
+    public void addInterest(float interestRate);
     public void generateMonthlyStatement(Customer customer);
     public void listActiveCustomers();
     public void generateDTER();
-    public void customerReport();
+    public void customerReport(Customer customer);
     public void deleteTransactions();
     
 }
--- Src/DAOImpl/ManagerInterfaceDAOImpl.java
+++ Src/DAOImpl/ManagerInterfaceDAOImpl.java
@@ -37,16 +37,29 @@ public class ManagerInterfaceDAOImpl implements ManagerInterfaceDAO{
         }
         return false;
     }
-    public void addInterest() {
+    public void addInterest(float interestRate) {
         // TODO: For all market accounts, add the appropriate amount of monthly interest to the balance.
         // This can only be done at the last business day of a month.
-        // 
+        // manager gets to set the interest rate
         Calendar calendar = Calendar.getInstance();
         int currentDay = calendar.get(Calendar.DAY_OF_MONTH);
         int lastDay = calendar.getActualMaximum(Calendar.DAY_OF_MONTH);
         if(currentDay != lastDay){
             System.out.println("ERROR: cannot add interest until the last day of the month.");
             return;
+        }
+        String query = "UPDATE Market_Accounts SET balance = balance * (1 + ?)";
+        try{
+            Connection connection = DbConnection.getConnection();
+            PreparedStatement statement = connection.prepareStatement(query);
+            
+            statement.setFloat(1, interestRate);
+            statement.executeUpdate();
+            System.out.println("Interest has been added to all market accounts.");
+        }catch(Exception e){
+            System.out.println("ERROR: adding interest failed.");
+            e.printStackTrace();
+            System.out.println(e);
         }
 
     }
@@ -84,10 +97,13 @@ public class ManagerInterfaceDAOImpl implements ManagerInterfaceDAO{
             ResultSet resultSet = statement.executeQuery();
 
             while(resultSet.next()){
-                System.out.println("Customer ID: " + resultSet.getString("customer_id"));
+                System.out.println("Customer ID: " + resultSet.getInt("customer_id"));
                 System.out.println("Total Buy Shares: " + resultSet.getString("total_buy_shares"));
                 System.out.println("Total Sell Shares: " + resultSet.getString("total_sell_shares"));
                 System.out.println("--------------------");
+            }
+            if(!resultSet.next()){
+                System.out.println("No active customers who have bought and sold 1000 shares found.");
             }
         }catch(Exception e){
             System.out.println("ERROR: listing active customers failed.");
@@ -97,12 +113,37 @@ public class ManagerInterfaceDAOImpl implements ManagerInterfaceDAO{
         
     }
     
-    public void customerReport(){
+    public void customerReport(Customer customer){
         //TODO:Generate a list of all accounts associated with a particular customer and the current balance
+        //query the db for all accounts associated with a particular customer and the current balance
+        String query = "SELECT * FROM Accounts WHERE username = ?";
+        try{
+            Connection connection = DbConnection.getConnection();
+            PreparedStatement statement = connection.prepareStatement(query);
+
+            statement.setString(1, customer.getUsername());
+
+            ResultSet resultSet = statement.executeQuery();
+
+            while(resultSet.next()){
+                System.out.println("Customer Name: " + resultSet.getString("name"));
+                System.out.println("Account Type: " + resultSet.getString("account_type"));
+                System.out.println("Balance: " + resultSet.getString("balance"));
+                System.out.println("--------------------");
+            }
+            if(!resultSet.next()){
+                System.out.println("No accounts associated with this customer found.");
+            }
+        }catch(Exception e){   
+            System.out.println("ERROR: generating customer report failed.");
+            e.printStackTrace();
+            System.out.println(e);
+        }
     }
     
     public void deleteTransactions(){
         //TODO: Delete the list of transactions from each of the accounts, in preparation for a new month of processing.
+        //should just be deleting every transaction 
         
         System.out.println("All transactions have been deleted.");
     }
@@ -113,7 +154,39 @@ public class ManagerInterfaceDAOImpl implements ManagerInterfaceDAO{
         // including earnings from buying/selling stocks and interest. The residence state of each customer should
         // also be listed
         // query the db for all customers who have made more than $10,000 in the last month
-        // print out the list of customers
+        Calendar calendar = Calendar.getInstance();
+        int currentMonth = calendar.get(Calendar.MONTH);
+        int currentYear = calendar.get(Calendar.YEAR);
+        String query = "SELECT c.customer_id, c.name, c.state, SUM(t.buy_shares) AS total_buy_shares, SUM(t.sell_shares) AS total_sell_shares, SUM(t.buy_shares * t.buy_price) AS total_buy_price, SUM(t.sell_shares * t.sell_price) AS total_sell_price " +
+                       "FROM Trades t " +
+                       "JOIN Customers c ON t.customer_id = c.customer_id " +
+                       "WHERE MONTH(t.trade_date) = ? AND YEAR(t.trade_date) = ? " +
+                       "GROUP BY c.customer_id " +
+                       "HAVING total_buy_price + total_sell_price >= 10000";
+        
+        try{
+            Connection connection = DbConnection.getConnection();
+            PreparedStatement statement = connection.prepareStatement(query);
+            
+            statement.setInt(1, currentMonth);
+            statement.setInt(2, currentYear);
+
+            ResultSet resultSet = statement.executeQuery();
+            while(resultSet.next()){
+                System.out.println("Customer ID: " + resultSet.getInt("customer_id"));
+                System.out.println("Customer Name: " + resultSet.getString("name"));
+                System.out.println("Residence State: " + resultSet.getString("state"));
+                System.out.println("--------------------");
+            }
+            if(!resultSet.next()){
+                System.out.println("No customers have made more than $10,000 in the last month.");
+            }
+        }catch(Exception e){
+            System.out.println("ERROR: generating DTER failed.");
+            e.printStackTrace();
+            System.out.println(e);
+        }
+        
     }
 
     public static void main(String[] args) {
@@ -121,6 +194,7 @@ public class ManagerInterfaceDAOImpl implements ManagerInterfaceDAO{
         // first manager needs to login
         ManagerInterfaceDAOImpl manager = new ManagerInterfaceDAOImpl();
         System.out.println("Welcome to the Manager Interface!");
+        System.out.println("---------------------------------");
         System.out.println("Please enter your username: ");
         Scanner scanner = new Scanner(System.in);
         while(true){
@@ -150,7 +224,10 @@ public class ManagerInterfaceDAOImpl implements ManagerInterfaceDAO{
         while(choice != 7){
             switch(choice){
                 case 1:
-                    manager.addInterest();
+                    System.out.println("Enter the interest rate as a decimal: ");
+                    float interestRate = scanner2.nextFloat();
+                    manager.addInterest(interestRate);
+
                     break;
                 case 2:
                     System.out.println("Enter the customer username: ");
@@ -165,16 +242,20 @@ public class ManagerInterfaceDAOImpl implements ManagerInterfaceDAO{
                     manager.generateDTER();
                     break;
                 case 5:
-                    manager.customerReport();
+                    System.out.println("Enter the customer username: ");
+                    String customerUsername2 = scanner2.nextLine(); 
+                    Customer customer2 = customerDAO.getCustomer(customerUsername2);
+                    manager.customerReport(customer2);
                     break;
                 case 6:
+                    System.out.println("Deleting all transactions...");
                     manager.deleteTransactions();
                     break;
                 default:
                     System.out.println("Invalid choice. Please try again.");
                     break;
             }
-            System.out.println("What would you like to do?");
+            System.out.println("What would you like to do next?");
             System.out.println("1. Add Interest");
             System.out.println("2. Generate Monthly Statement");
             System.out.println("3. List Active Customers");
@@ -184,6 +265,7 @@ public class ManagerInterfaceDAOImpl implements ManagerInterfaceDAO{
             System.out.println("7. Exit");
             choice = scanner.nextInt();
         }
+        System.out.println("Goodbye!");
         //close scanner when 7 is selected
         scanner.close();
         scanner2.close();
